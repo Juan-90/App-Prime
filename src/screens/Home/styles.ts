3b274f16d75d340078c12c5f213370a7b8b3616a
Juan-90 <juan.andrade817@gmail.com>
2024-11-21 16:07:41 -0300

    Inicio do projeto e tela home

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#1e1e1e',
+      padding: 25,
+    },
+
+    headerText: {
+        marginTop: 30,
+        fontSize: 24,
+        lineHeight: 45,
+        color: '#fff',
+    },
+
+    containerInput: {
+        backgroundColor: '#67686D',
+        height: 42,
+        padding: 18,
+        borderRadius: 16,
+        marginTop: 24,
+        marginBottom: 28,
+    }
+
+  });
